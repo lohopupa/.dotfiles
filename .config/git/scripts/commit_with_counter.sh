88a71d64ebfe2519567c6d7c8d3b522b829eec14
Lohopupa <lohopupa@gmail.com>
2025-05-10 02:22:34 +0600

    Added feature #2: Added new git aliases for feature, bug and test commits

---
+++ .config/git/scripts/commit_with_counter.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+type="$1"
+shift
+
+count=$(git config --local --get counters."$type")
+count=${count:-0}
+next=$((count + 1))
+prefix=""
+
+case "$type" in
+  bug) prefix="Fixed bug" ;;
+  feature) prefix="Added feature" ;;
+  chore) prefix="Performed chore" ;;
+  test) prefix="Testing code" ;;
+  *) echo "Unknown type: $type" >&2; exit 1 ;;
+esac
+
+msg="$prefix #$next"
+if [[ -n "$1" ]]; then
+  msg="$msg: $*"
+fi
+
+if git commit -m "$msg"; then
+  git config --local counters."$type" "$next"
+fi
